94424e8fe0b3d1007503b15124d9c825dd92897d
Krzysztof Fryc <45319085+Goriath@users.noreply.github.com>
2019-02-11 14:20:31 +0100

    Working with multiple Threads
    - dealing with deadlocks
    - synchronization
    - notify and notifyAll
    - wait

---
+++ Counter/src/com/kfryc/Main.java
@@ -0,0 +1,60 @@
+package com.kfryc;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Countdown countdown = new Countdown();
+        CountdownThread t1 = new CountdownThread(countdown);
+        t1.setName("Thread 1");
+        CountdownThread t2 = new CountdownThread(countdown);
+        t2.setName("Thread 2");
+
+        t1.start();
+        t2.start();
+    }
+
+
+}
+
+class Countdown {
+    private int i;
+
+    // To avoid thread race condition we use "synchronized" keyword to enable one thread to finish their method
+    // and suspending other threads that want to use this method
+    // primitive types cannot be synchronized, only objects that the threads share in heap memory
+    // (String color is a local thread variable, so it will not work)
+
+    public void doCountdown() {
+        String color;
+
+        switch (Thread.currentThread().getName()) {
+            case "Thread 1":
+                color = ThreadColor.ANSI_CYAN;
+                break;
+            case "Thread 2":
+                color = ThreadColor.ANSI_PURPLE;
+                break;
+            default:
+                color = ThreadColor.ANSI_GREEN;
+        }
+
+        // we can synchronize objects, static methods and static objects
+        synchronized (this) {
+            for (i = 10; i > 0; i--) {
+                System.out.println(color + Thread.currentThread().getName() + ": i = " + i);
+            }
+        }
+    }
+}
+
+class CountdownThread extends Thread {
+    private Countdown threadCountdown;
+
+    public CountdownThread(Countdown countdown) {
+        this.threadCountdown = countdown;
+    }
+
+    public void run() {
+        threadCountdown.doCountdown();
+    }
+}
