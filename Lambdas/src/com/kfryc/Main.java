68bd9459c7e6d04aa117d6a467baf67f36438839
Krzysztof Fryc <45319085+Goriath@users.noreply.github.com>
2019-02-14 19:17:08 +0100

    Lambdas

--- Lambdas/src/com/kfryc/Main.java
+++ Lambdas/src/com/kfryc/Main.java
@@ -56,8 +56,29 @@ public class Main {
             System.out.println(employee.getName());
         }
 
+        // Option 1
+//        String sillyString = doStringStuff(new UpperConcat() {
+//            @Override
+//            public String upperAndConcat(String s1, String s2) {
+//                return s1.toUpperCase() + s2.toUpperCase();
+//            }
+//        }, employees.get(0).getName(), employees.get(1).getName());
+//        System.out.println(sillyString);
+
+        // Option 2 using lambda
+        UpperConcat uc = ( s1,  s2) -> s1.toUpperCase() + s2.toUpperCase();
+        String sillyString = doStringStuff(uc, employees.get(0).getName(), employees.get(1).getName());
+        System.out.println(sillyString);
+
+
+    }
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2){
+        return uc.upperAndConcat(s1, s2);
     }
 }
+
+
 
 class Employee{
     private String name;
@@ -83,6 +104,10 @@ class Employee{
     public void setAge(int age) {
         this.age = age;
     }
+}
+
+interface UpperConcat{
+    public String upperAndConcat(String s1, String s2);
 }
 
 // To option 1
