e12bd4b870a42d3c3569cf03baa0a2079f9428fd
Krzysztof Fryc <45319085+Goriath@users.noreply.github.com>
2019-02-14 18:33:15 +0100

    Lambda expression example in JavaFX

---
+++ Lambdas/src/com/kfryc/Main.java
@@ -0,0 +1,94 @@
+package com.kfryc;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Option 1
+//	    new Thread(new CodeToRun()).start();
+
+        //Option 2
+//	    new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("Printing from the Runnable");
+//            }
+//        }).start();
+
+        // Option 3 using lambda
+        new Thread(() -> {
+            System.out.println("Printing from the Runnable");
+            System.out.println("Line 2");
+            System.out.format("This is line %d\n",3);
+        }).start();
+
+        Employee pawel = new Employee("Pawel Nowak", 35);
+        Employee krzysztof = new Employee("Krzysztof Fryc", 32);
+        Employee magda = new Employee("Magda Kowalska", 30);
+        Employee anna = new Employee("Anna Rodo", 28);
+
+
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(pawel);
+        employees.add(krzysztof);
+        employees.add(magda);
+        employees.add(anna);
+
+
+        // Option 1
+//        Collections.sort(employees, new Comparator<Employee>() {
+//            @Override
+//            public int compare(Employee employee1, Employee employee2) {
+//                return employee1.getName().compareTo(employee2.getName());
+//            }
+//        });
+
+        // Option 2 using lambda - compiler knows that Employee class is being used
+        Collections.sort(employees, (employee1, employee2) ->
+                employee1.getName().compareTo(employee2.getName()));
+
+        for(Employee employee : employees){
+            System.out.println(employee.getName());
+        }
+
+    }
+}
+
+class Employee{
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+// To option 1
+//class CodeToRun implements Runnable {
+//    @Override
+//    public void run() {
+//        System.out.println("Printing from the Runnable");
+//    }
+//}
